ef8a0e1e904f789aecd3acd8d5e2aaa50dc9ed8b
OEM <gurleva_ludmila@mail.ru>
2023-07-02 00:35:59 +0300

    First files

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post(); //post: Post@772
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
+//Вашей задачей будет разработать классы,описывающие объекты запроса анкеты на сервер,
+// и продемонстрировать их работу в Main.
